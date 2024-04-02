b34ac44eeb364de785281f404934d5f8f69636d1
palaceparis <palaceparisg@outlook.com>
2024-04-02 11:14:06 +0000

    move old foldr to cv example and remove vis and modify run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-# Replace /path/to/conda with the actual path to your conda installation
 source /hpc2hdd/home/mgong081/anaconda3/etc/profile.d/conda.sh
-
-# Activate your conda environment
 conda activate PytorchDeepSynergy
 
-# List of dataset files to iterate over
-datasets=("3mlp_data_test_fold2_tanh.p" "3mlp_data_test_fold3_tanh.p" "3mlp_data_test_fold4_tanh.p")
+# Lists to run
+datasets=("data_test_fold0_tanh.p" "data_test_fold1_tanh.p" "data_test_fold2_tanh.p" "data_test_fold3_tanh.p" "data_test_fold4_tanh.p")
+models=("deepSynergy" "3MLP" "matchMaker")
 
-# Base path to the datasets
-base_path="/hpc2hdd/home/mgong081/Projects/DeepSynergy/data"
-
-for dataset in "${datasets[@]}"; do
-    # Construct the full path to the dataset
-    data_path="$base_path/$dataset"
-    
-    # Run the Python script with the current dataset
-    python src/model/3MLP_deepSynergy/v01_run_moving_average.py data.file="$data_path"
+for model in "${models[@]}"; do
+    for dataset in "${datasets[@]}"; do
+        dataset="/hpc2hdd/home/mgong081/Projects/DeepSynergy/data/$dataset"
+        python src/model/run.py --epochs 2 --model $model --data_file=$dataset
+    done
 done
